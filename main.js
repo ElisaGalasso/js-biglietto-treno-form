38b2e2b9081aafce54f0328f1998caa0c077e5af
“ElisaGalasso“ <galassoelisa72@gmail.com>
2025-07-23 19:06:26 +0200

    completata logica calcolo biglietto e input

--- main.js
+++ main.js
@@ -11,30 +11,34 @@ const stampaOutput = document.getElementById ('output')
 
 buttonEl.addEventListener ('click', function () {
 
+    //al click prendi questi valori
+
     const età = Number (etàInput.value)
     const km = Number (kmInput.value)
 
     //calcolo prezzo biglietto
 
-    let sconto = ""
-    let biglietto = costoAlKm * passengerKm
+    let sconto = 0
+    let biglietto = costoAlKm * km
+
+    //calcolo sconto
 
 if (età < 18) {
     sconto = biglietto * 0.20
     biglietto = biglietto - sconto
-    console.log(biglietto);
+    console.log(biglietto.toFixed (2));
     
     
-} else if (età > 65) {
+} else if (età >= 65) {
     sconto = biglietto * 0.40
     biglietto = biglietto - sconto
-    console.log(biglietto);
+    console.log(biglietto.toFixed (2));
     
 } else {
-    console.log(biglietto);
+    console.log(biglietto.toFixed (2));
     
 }
-    
+    stampaOutput.textContent = `Prezzo biglietto: € ${biglietto.toFixed(2)}`
     console.log('Prezzo biglietto:', biglietto.toFixed(2));
 })
  
